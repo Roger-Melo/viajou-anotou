92deaab89f0a7ddabd6cd3865365bf4b2269ab60
Roger-Melo <rogerw592@gmail.com>
2024-01-07 23:05:00 -0300

    ✅ Create Pricing Page

--- src/app.jsx
+++ src/app.jsx
@@ -10,7 +10,8 @@ import {
 
 const links = [
   { path: '/', text: 'Início' },
-  { path: '/sobre', text: 'Sobre' }
+  { path: '/sobre', text: 'Sobre' },
+  { path: '/preco', text: 'Preço' }
 ]
 
 const Header = () => {
@@ -67,11 +68,26 @@ const About = () =>
     </main>
   </>
 
+const Pricing = () =>
+  <>
+    <Header />
+    <main className="main-pricing">
+      <section>
+        <div>
+          <h1>Preço simples.<br />Só R$ 47/mês.</h1>
+          <p>Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o quê você fez pelo mundo.</p>
+        </div>
+        <img src="/preco-viajou-anotou.jpg" alt="Avenida Paulista" />
+      </section>
+    </main>
+  </>
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<Home />} />
       <Route path="sobre" element={<About />} />
+      <Route path="preco" element={<Pricing />} />
     </Route>
   )
 )
