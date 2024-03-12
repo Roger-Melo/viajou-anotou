58fe707ecaf8d42837519071dc746d900f84e6fd
Roger-Melo <rogerw592@gmail.com>
2024-03-12 15:09:28 -0300

    ✅ Create home file

--- src/app.jsx
+++ src/app.jsx
@@ -19,22 +19,11 @@ import {
   Navigate,
   Form
 } from 'react-router-dom'
+import { Home } from '@/pages/home'
 import { About } from '@/pages/about'
 import { Pricing } from '@/pages/pricing'
 import { Header } from '@/ui/header'
 import { Logo } from '@/ui/logo'
-
-const Home = () =>
-  <>
-    <Header />
-    <main className="main-home">
-      <section>
-        <h1>Você viaja o mundo.<br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
-        <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
-        <Link to="/app" className="cta">Começar agora</Link>
-      </section>
-    </main>
-  </>
 
 const NotFound = () =>
   <>
